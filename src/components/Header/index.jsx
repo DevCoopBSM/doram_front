402f148d984fd282d7ecd9af6e92cb7246f9ebc5
Middle1204 <guu0414a@gmail.com>
2024-10-28 22:07:28 +0900

    fix : header 위치 수정

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -31,9 +31,7 @@ const UserHeader = () => {
       <Logo src={logo} alt="Logo" onClick={handleLogoClick} />
       <SearchBar placeholder="검색어를 입력하세요" />
       <WriteButton onClick={handleWriteClick}>글 작성</WriteButton>
-      <AlarmButton>
-        <img src={alarmIcon} alt="Alarm" />
-      </AlarmButton>
+      
       <UserProfile onClick={handleProfileClick}>
         <img src={userImage} alt="User Profile" />
         <span>UserName</span>
@@ -44,6 +42,9 @@ const UserHeader = () => {
           </DropdownMenu>
         )}
       </UserProfile>
+      <AlarmButton>
+        <img src={alarmIcon} alt="Alarm" />
+      </AlarmButton>
     </HeaderContainer>
   );
 };
