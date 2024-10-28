4c04d83fd8f751b11b6c23c1d5bd70389a88b87f
E-jiwoo <livei07@naver.com>
2024-10-29 07:41:20 +0900

    .

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import * as S from "./style";
 import UserHeader from "../Header";
-import logo from "../../assets/doram.svg";
+import logo from "../../assets/doram.png";
 
 const Modify = () => {
   return (
     <S.LayoutContainer>
       <UserHeader />
       <S.ContentContainer>
-        <S.Logo src={logo} aly="logo" />
+        <S.Contain>
+          <S.Logo src={logo} aly="logo" />
+          <S.Text>회원가입 후 도람의 더 다양한 서비스를 만나보세요!</S.Text>
+        </S.Contain>
       </S.ContentContainer>
     </S.LayoutContainer>
   );
--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -12,9 +12,25 @@ export const LayoutContainer = styled.div`
 export const ContentContainer = styled.div`
   width: 100%;
   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Contain = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Logo = styled.img`
+  margin-top: 100px;
   width: 263px;
   height: 85px;
+`;
+
+export const Text = styled.div`
+  color: "black";
+  font-size: 24px;
+  font-weight: 400;
+  margin-top: 30px;
+  white-space: nowrap;
 `;
