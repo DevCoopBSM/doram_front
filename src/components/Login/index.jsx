3c7990a90ade8f67a4893a8dde57647cd40beaff
E-jiwoo <livei07@naver.com>
2024-10-30 00:40:02 +0900

    add :: login page - 로그인 페이지 퍼블리싱

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -3,7 +3,7 @@ import * as S from "./style";
 import UserHeader from "../Header";
 import logo from "../../assets/doram.png";
 
-const Modify = () => {
+const Login = () => {
   return (
     <S.LayoutContainer>
       <UserHeader />
@@ -11,10 +11,21 @@ const Modify = () => {
         <S.Contain>
           <S.Logo src={logo} aly="logo" />
           <S.Text>회원가입 후 도람의 더 다양한 서비스를 만나보세요!</S.Text>
+          <S.InputSection>
+            <S.Input placeholder="아이디를 입력해 주세요." />
+            <S.Input placeholder="이름을 입력해 주세요." />
+            <S.Input placeholder="이메일을 입력해 주세요." />
+            <S.Input placeholder="비밀번호를 입력해 주세요." />
+          </S.InputSection>
+          <S.LoginSection>
+            <S.Text2>만약 계정이 있다면?</S.Text2>
+            <S.Login>로그인하기</S.Login>
+          </S.LoginSection>
+          <S.AccountBtn>회원가입하기</S.AccountBtn>
         </S.Contain>
       </S.ContentContainer>
     </S.LayoutContainer>
   );
 };
 
-export default Modify;
+export default Login;
--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -19,10 +19,11 @@ export const ContentContainer = styled.div`
 export const Contain = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 export const Logo = styled.img`
-  margin-top: 100px;
+  margin-top: 40px;
   width: 263px;
   height: 85px;
 `;
@@ -32,5 +33,54 @@ export const Text = styled.div`
   font-size: 24px;
   font-weight: 400;
   margin-top: 30px;
+  margin-bottom: 30px;
   white-space: nowrap;
+`;
+
+export const InputSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const Input = styled.input`
+  width: 700px;
+  color: #808080;
+  font-size: 24px;
+  font-weight: 400;
+  background-color: #f2f2f2;
+  white-space: nowrap;
+  padding-left: 24px;
+`;
+
+export const LoginSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  margin-top: 20px;
+`;
+
+export const Text2 = styled.div`
+  color: #41434c;
+  font-size: 24px;
+  font-weight: "400";
+  word-wrap: "break-word";
+`;
+
+export const Login = styled.div`
+  color: #0085ff;
+  font-size: 24px;
+  font-weight: "400";
+  word-wrap: "break-word";
+`;
+
+export const AccountBtn = styled.button`
+  margin-top: 20px;
+  width: 700px;
+  height: 77px;
+  background-color: #8ee559;
+  color: #ffffff;
+  font-size: 32px;
+  font-weight: "400";
+  word-wrap: "break-word";
 `;
